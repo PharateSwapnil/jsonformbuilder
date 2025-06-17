981add472cf57184ed872ba286b870bfeb229425
pharateswapnil <pharateswapnil4141@gmail.com>
2025-06-17 12:45:07 +0530

    change port

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
       : 5000;
   
   // Use 0.0.0.0 for external accessibility in Replit
-  const host = "0.0.0.0";
+  const host = "localhost";
   
   // Handle port conflicts gracefully
   server.listen(port, host, () => {
